b635092da58fe7cae5c93617621594fba85495b6
PavelNovotny <pavel.novotny.cz@gmail.com>
2010-10-29 23:00:50 +0200

    'Builder'

---
+++ Builder/src/cz/pn/patterns/builder/App.java
@@ -0,0 +1,14 @@
+package cz.pn.patterns.builder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pavel
+ * Date: 29.10.2010
+ * Time: 10:06:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class App {
+    public static void main (String[] args) {
+        
+    }
+}
---
+++ Builder/src/cz/pn/patterns/builder/NutritionFacts.java
@@ -0,0 +1,16 @@
+package cz.pn.patterns.builder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pavel
+ * Date: 29.10.2010
+ * Time: 12:15:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class NutritionFacts {
+    private String water;
+    private String carbohydrate;
+    private String sodium;
+    private String salt;
+    private String fat;
+}
